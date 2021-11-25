634e8670d6f899fb51481373ca64fd6f344d9d6a
Iris Smok <iriscemeljic11@gmail.com>
2021-11-25 21:20:33 +0000

    Add code for flip and chech if cards are matching

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,49 @@
 let cards = document.querySelectorAll('.memory-card');
 
-function flipCard(){
-   this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+
+function flipCard(){
+   this.classList.add('flip');
+
+   if(!hasFlippedCard){
+
+      hasFlippedCard = true;
+      firstCard = this;
+
+   } else {
+      hasFlippedCard = false;
+      secondCard = this;
+
+      checkFroMarch();
+     
 }
+}
+
+
+function checkFroMarch(){
+   if (firstCard.dataset.card === secondCard.dataset.card){
+         
+      disableCards();
+   } else{
+
+   unflipCards();
+   }
+}
+
+
+function disableCards(){
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+   setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+   }, 1500);
+}
+
+
 cards.forEach(card => card.addEventListener('click', flipCard ));

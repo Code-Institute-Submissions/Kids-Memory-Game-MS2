9e689c32a080ad9e65cede939213453b0c66c8a7
Iris Smok <iriscemeljic11@gmail.com>
2021-12-03 01:13:45 +0000

    Add matching function to memory game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
 const cards = document.querySelectorAll('.cards');
-
-let clickedCard = null;
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
 
 
 function flipCard(){
-    this.classList.add('flip');
-   console.log(this.getAttribute('data-img'));
+   
    this.classList.remove('color-hidden');
+
+   if(!hasFlippedCard){
+       hasFlippedCard = true;
+       firstCard = this;
+   } else{
+       hasFlippedCard = false;
+       secondCard = this;
+
+    if (firstCard.dataset.img === secondCard.dataset.img){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    } else{
+        setTimeout(() => {
+        firstCard.classList.add('color-hidden');
+        secondCard.classList.add('color-hidden');
+    }, 1500); }
+
+    console.log(firstCard, secondCard);
+   }
 
 }
 cards.forEach (card => card.addEventListener('click', flipCard));

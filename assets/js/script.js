38f3e9f2eb5728f926af9c1ab87b422b8a0c2f81
Iris Smok <iriscemeljic11@gmail.com>
2021-12-03 01:31:29 +0000

    Add lockBoard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const cards = document.querySelectorAll('.cards');
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard;
 let secondCard;
 
 
 function flipCard(){
-   
+   if (lockBoard) return;
+
    this.classList.remove('color-hidden');
 
    if(!hasFlippedCard){
@@ -15,20 +17,43 @@ function flipCard(){
        hasFlippedCard = false;
        secondCard = this;
 
-    if (firstCard.dataset.img === secondCard.dataset.img){
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
-    } else{
-        setTimeout(() => {
-        firstCard.classList.add('color-hidden');
-        secondCard.classList.add('color-hidden');
-    }, 1500); }
-
-    console.log(firstCard, secondCard);
+   
+matchingCards();
+    
    }
 
 }
+
+function matchingCards(){
+
+    if (firstCard.dataset.img === secondCard.dataset.img){
+
+        disableCards();
+    } else {
+
+            unflipCards();
+    }
+
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    }
+
+
+function unflipCards(){
+lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.add('color-hidden');
+        secondCard.classList.add('color-hidden');
+        lockBoard = false;
+    }, 1500); 
+}
+
+
 cards.forEach (card => card.addEventListener('click', flipCard));
 
 

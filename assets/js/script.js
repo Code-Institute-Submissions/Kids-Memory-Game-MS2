70e6001e12738786fc1c57acae7fe66518416eaa
Iris Smok <iriscemeljic11@gmail.com>
2021-12-11 05:16:50 +0000

    Format JS with js formatter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 let card = document.getElementsByClassName("card");
 let cards = [...card];
 let randomNum = null;
 let moves = 0;
-let counter = document.querySelector(".moves");
+let counter = document.querySelector(".moves"); // MOVES COUNTER
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard;
 let secondCard;
-let matchCounter = 0; 
+let matchCounter = 0;
 
 const modalBtn = document.getElementById("modalBtn"); // HOW TO PLAY BTN
 const closeBtn = document.getElementById("closeBtn"); // HOW TO PLAY CLOSE WINDOW
@@ -25,8 +25,8 @@ const animals = [
     'lionnew',
     'sheep',
     'bird',
-    
-  ];
+
+];
 
 
 window.onload = startGame();
@@ -34,32 +34,32 @@ window.onload = startGame();
 
 
 // loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-   cards[i].addEventListener("click", flipCard);
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", flipCard);
 }
 
 
 // DID YOU KNOW SHOW LESS -SHOW MORE
 
-function didYouKnow(){
+function didYouKnow() {
     const know = document.getElementById('wrapper');
     var x = window.matchMedia("(max-width: 1000px)"); // MEDIA QUERY
     var moreText = document.getElementById("more");
 
-  var btnText = document.getElementById("myBtn");
+    var btnText = document.getElementById("myBtn");
 
-  if (moreText.style.display === "none") {
-    btnText.innerHTML = "Show Less"; 
-    moreText.style.display = "inline-block";
-    if (x.matches) { // If media query matches
-        know.style.width = "90%";
-      }
-      
-  } else {
-    btnText.innerHTML = "Show More"; 
-    moreText.style.display = "none";
-    know.style.width = "fit-content";
-  }
+    if (moreText.style.display === "none") {
+        btnText.innerHTML = "Show Less";
+        moreText.style.display = "inline-block";
+        if (x.matches) { // If media query matches
+            know.style.width = "90%";
+        }
+
+    } else {
+        btnText.innerHTML = "Show More";
+        moreText.style.display = "none";
+        know.style.width = "fit-content";
+    }
 }
 
 
@@ -80,56 +80,58 @@ function closeInstructions() {
 
 
 // FLIP CARD FUNCTION
-function flipCard(){
-   if (lockBoard) return;
+function flipCard() {
+    if (lockBoard) return;
 
-   if(this === firstCard) return;
+    if (this === firstCard) return;
 
-   this.classList.remove('color-hidden');
+    this.classList.remove('color-hidden');
 
-   if(!hasFlippedCard){
-       hasFlippedCard = true;
-       firstCard = this;
-   } else{
-       hasFlippedCard = false;
-       secondCard = this;
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
 
-   }
-   moveCounter();
-   matchingCards();
+    }
+    moveCounter();
+    matchingCards();
 }
 
 
 
 
 //MATCHING CARDS
-function matchingCards(){
-   
-    if (firstCard.dataset.img === secondCard.dataset.img){
-       
-        matchCounter+=1;
+function matchingCards() {
+
+    if (firstCard.dataset.img === secondCard.dataset.img) {
+
+        matchCounter += 1;
         disableCards();
-        if(matchCounter==(cards.length/2)){
+        if (matchCounter == (cards.length / 2)) {
             clearInterval(interval);
-        finalTime = timer.innerHTML;
-    //show congratulations modal
-    modal.classList.add("show");
-    //showing move, rating, time on modal
-    document.getElementById("finalMove").innerHTML = moves;
-    document.getElementById("totalTime").innerHTML = finalTime;
-    //closeicon on modal
-    closeModal();
+            finalTime = timer.innerHTML;
+            //show congratulations modal
+            modal.classList.add("show");
+            //showing move, rating, time on modal
+            document.getElementById("finalMove").innerHTML = moves;
+            document.getElementById("totalTime").innerHTML = finalTime;
+            //closeicon on modal
+            closeModal();
+        }
+
+    } else {
+        unflipCards();
     }
-    
-    } else {   unflipCards(); }
-   
+
 }
 
 
 // CONGRATULATIONS
 
-function closeModal(){
-    closeicon.addEventListener("click", function(){
+function closeModal() {
+    closeicon.addEventListener("click", function() {
         modal.classList.remove("show");
         startGame();
         matchCounter = 0;
@@ -137,7 +139,7 @@ function closeModal(){
 }
 
 //for player to play Again 
-function playAgain(){
+function playAgain() {
     modal.classList.remove("show");
     startGame();
     matchCounter = 0;
@@ -145,91 +147,92 @@ function playAgain(){
 
 
 // DISABLE CARDS
-function disableCards(){
+function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-resetBoard();
-    }
+    resetBoard();
+}
 
 // UNFLIP CARDS
-function unflipCards(){
-lockBoard = true;
+function unflipCards() {
+    lockBoard = true;
 
     setTimeout(() => {
         firstCard.classList.add('color-hidden');
         secondCard.classList.add('color-hidden');
         resetBoard();
-    }, 1500); 
+    }, 1500);
 }
 
 //RESET BOARD
-function resetBoard(){
- [hasFlippedCard, lockBoard] = [false, false];
- [firstCard, secondCard] = [null, null];
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 
 
 }
 
 // FUNCTION SHUFFLE
 function shuffleAnimals() {
-    
+
     for (let animal of animals) {
         const cardAIndex = parseInt(Math.random() * randomNum.length);
         const cardA = randomNum[cardAIndex];
         randomNum.splice(cardAIndex, 1);
-    
-        if(cardA.getAttribute('data-img')?.length>0)
-                cardA.classList.remove(cardA.getAttribute('data-img'));
-         cardA.classList.add(animal);
+
+        if (cardA.getAttribute('data-img')?.length > 0)
+            cardA.classList.remove(cardA.getAttribute('data-img'));
+        cardA.classList.add(animal);
         cardA.setAttribute('data-img', animal);
-    
+
         const cardBIndex = parseInt(Math.random() * randomNum.length);
         const cardB = randomNum[cardBIndex];
         randomNum.splice(cardBIndex, 1);
-    
-        if(cardB.getAttribute('data-img')?.length>0)
-                cardB.classList.remove(cardB.getAttribute('data-img'));
+
+        if (cardB.getAttribute('data-img')?.length > 0)
+            cardB.classList.remove(cardB.getAttribute('data-img'));
         cardB.classList.add(animal);
-    
+
         cardB.setAttribute('data-img', animal);
-    
-    
-        }
+
+
     }
- 
+}
 
 
-  // MOVES COUNT
 
-function moveCounter(){    
-    moves++;    
+// MOVES COUNT
+
+function moveCounter() {
+    moves++;
     counter.innerHTML = moves;
 
-    if(moves === 1){
+    if (moves === 1) {
         second = 0;
         minute = 0;
-startTimer();
-        
+        startTimer();
+
     }
 }
 
 // TIMER
-var second = 0, minute = 0; 
+var second = 0,
+    minute = 0;
 var timer = document.querySelector(".timer");
 var interval;
 
-function startTimer(){
-    interval = setInterval(function(){
-        timer.innerHTML = minute + " mins "+ second + " secs";
-        second++; 
-        if(second == 60){
+function startTimer() {
+    interval = setInterval(function() {
+        timer.innerHTML = minute + " mins " + second + " secs";
+        second++;
+        if (second == 60) {
             minute++;
             second = 0;
         }
-        if(minute == 60){
+        if (minute == 60) {
             minute = 0;
         }
-    },1000);
+    }, 1000);
 }
 
 
@@ -238,7 +241,7 @@ function startTimer(){
 
 function startGame() {
 
-     
+
     randomNum = [...document.querySelectorAll('.random')];
 
     for (var i = 0; i < cards.length; i++) {
@@ -246,7 +249,7 @@ function startGame() {
 
         cards[i].removeEventListener('click', flipCard);
         cards[i].addEventListener('click', flipCard);
-       
+
     }
     firstCard = null;
 
@@ -261,9 +264,7 @@ function startGame() {
     var timer = document.querySelector(".timer");
     timer.innerHTML = "0 mins 0 secs";
     clearInterval(interval);
-    
+
     shuffleAnimals();
     resetBoard();
 }
-
-
